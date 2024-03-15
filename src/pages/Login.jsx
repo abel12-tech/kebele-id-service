adf3e7afce74ea51ad1aa69640ae15410045be91
abel <abelkinfu678@gmail.com>
2024-03-15 11:40:33 +0300

    register ,login and contact page is edited

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,56 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 p-16">
-      <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-        <h1 className="text-2xl mb-6 text-center">Sign in to your account</h1>
-        <form className="space-y-4">
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-gray-800 font-semibold mb-2"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="w-full outline-none border border-gray-300 p-2 rounded-lg"
-              placeholder="Enter your username"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-gray-800 font-semibold mb-2"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full outline-none border border-gray-300 p-2 rounded-lg"
-              placeholder="Enter your password"
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full rounded-lg bg-[#FDC351] text-gray-600 hover:bg-[#d1ae67] px-4 py-2"
-            >
-              Login
-            </button>
-          </div>
-        </form>
+    <>
+    <Navbar/>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 p-16">
+        <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+          <h1 className="text-2xl mb-6 text-center">Sign in to your account</h1>
+          <form className="space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-gray-800 font-semibold mb-2"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="w-full outline-none border border-gray-300 p-2 rounded-lg"
+                placeholder="Enter your username"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-gray-800 font-semibold mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full outline-none border border-gray-300 p-2 rounded-lg"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full rounded-lg bg-[#FDC351] text-gray-600 hover:bg-[#d1ae67] px-4 py-2"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,101 +1,105 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Register = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 p-16">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl mb-6 text-center">Register</h1>
-        <form className="space-y-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label
-                htmlFor="firstname"
-                className="block text-gray-800 font-semibold mb-2"
-              >
-                First Name
-              </label>
-              <input
-                type="text"
-                id="firstname"
-                name="firstname"
-                className="w-full outline-none border border-gray-300 p-2 rounded-lg"
-                placeholder="Enter your first name"
-              />
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 p-16">
+        <div className="bg-white px-4 py-4 rounded-lg shadow-md">
+          <h1 className="text-2xl mb-6 text-center">Register</h1>
+          <form className="space-y-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+              <div>
+                <label
+                  htmlFor="firstname"
+                  className="block text-gray-800 font-semibold mb-2"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="firstname"
+                  name="firstname"
+                  className="w-full outline-none border border-gray-300 p-2 rounded-lg"
+                  placeholder="Enter your first name"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="lastname"
+                  className="block text-gray-800 font-semibold mb-2"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  id="lastname"
+                  name="lastname"
+                  className="w-full outline-none border border-gray-300 p-2 rounded-lg"
+                  placeholder="Enter your last name"
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+              <div>
+                <label
+                  htmlFor="phonenumber"
+                  className="block text-gray-800 font-semibold mb-2"
+                >
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  id="phonenumber"
+                  name="phonenumber"
+                  className="w-full outline-none border border-gray-300 p-2 rounded-lg"
+                  placeholder="Enter your phone number"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="image"
+                  className="block text-gray-800 font-semibold mb-2"
+                >
+                  Image
+                </label>
+                <input
+                  type="file"
+                  id="image"
+                  name="image"
+                  accept="image/*"
+                  className="w-full outline-none border border-gray-300 p-2 rounded-lg"
+                />
+              </div>
             </div>
             <div>
               <label
-                htmlFor="lastname"
+                htmlFor="email"
                 className="block text-gray-800 font-semibold mb-2"
               >
-                Last Name
+                Email
               </label>
               <input
-                type="text"
-                id="lastname"
-                name="lastname"
+                type="email"
+                id="email"
+                name="email"
                 className="w-full outline-none border border-gray-300 p-2 rounded-lg"
-                placeholder="Enter your last name"
-              />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label
-                htmlFor="phonenumber"
-                className="block text-gray-800 font-semibold mb-2"
-              >
-                Phone Number
-              </label>
-              <input
-                type="text"
-                id="phonenumber"
-                name="phonenumber"
-                className="w-full outline-none border border-gray-300 p-2 rounded-lg"
-                placeholder="Enter your phone number"
+                placeholder="Enter your email address"
               />
             </div>
             <div>
-              <label
-                htmlFor="image"
-                className="block text-gray-800 font-semibold mb-2"
+              <button
+                type="submit"
+                className="w-full rounded-lg bg-[#FDC351] text-gray-600 hover:bg-[#d1ae67] px-4 py-2"
               >
-                Image
-              </label>
-              <input
-                type="file"
-                id="image"
-                name="image"
-                accept="image/*"
-                className="w-full outline-none border border-gray-300 p-2 rounded-lg"
-              />
+                Register
+              </button>
             </div>
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-gray-800 font-semibold mb-2"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full outline-none border border-gray-300 p-2 rounded-lg"
-              placeholder="Enter your email address"
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full rounded-lg bg-[#FDC351] text-gray-600 hover:bg-[#d1ae67] px-4 py-2"
-              >
-              Register
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
